cd143e2d2ca012119e29ff11618f3112bf127112
Jody <jlerch@sortimo.de>
2017-09-13 16:32:56 +0200

    npm pass arguments and set globals

--- globals.js
+++ globals.js
@@ -1,17 +1,38 @@
+const ARGS = process.argv.slice(2);
+
+switch (ARGS[0]) {
+    case "codeninja" :
+        var sshPrivateKeyPath = '/data/ssh/id_rsa'
+        var host = '172.22.2.158';
+        var username = 'sortimo';
+        var pathTest = '/home/sortimo/sprachtool-files/test';
+        var pathLive = '/home/sortimo/sprachtool-files/live';
+    break;
+    case "local" :
+        var sshPrivateKeyPath = 'C:/Users/jlerch/.ssh/private_key.ppk';
+        var host = '172.22.2.158';
+        var username = 'sortimo';
+        var pathTest = '/home/sortimo/sprachtool-files/test';
+        var pathLive = '/home/sortimo/sprachtool-files/live';
+    break;
+    default:
+
+}
+
 var Globals = {
     // url: 'mongodb://127.0.0.1:27017/myproject'
     url: 'mongodb://172.22.2.159:27017/myproject',
-    sshPrivateKeyPath: 'C:/Users/jlerch/.ssh/private_key.ppk',
+    sshPrivateKeyPath: sshPrivateKeyPath,
     sshDestinations: {
         test: {
-            host: '172.22.2.159',
-            username: 'sortimo',
-            path: '/home/sortimo/sprachtool-files/test'
+            host: host,
+            username: username,
+            path: pathTest
         },
         live: {
-            host: '172.22.2.159',
-            username: 'sortimo',
-            path: '/home/sortimo/sprachtool-files/live'
+            host: host,
+            username: username,
+            path: pathLive
         }
     }
 }
