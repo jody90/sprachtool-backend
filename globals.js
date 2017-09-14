adb2bc97524d1226aa66fd7d3e53b627d4a94f92
Jody <jlerch@sortimo.de>
2017-09-14 07:43:11 +0200

    mongodb host mapped to docker image name

--- globals.js
+++ globals.js
@@ -20,8 +20,7 @@ switch (ARGS[0]) {
 }
 
 var Globals = {
-    // url: 'mongodb://127.0.0.1:27017/myproject'
-    url: 'mongodb://172.22.2.159:27017/myproject',
+    url: 'mongodb://mongodb:27017/myproject',
     sshPrivateKeyPath: sshPrivateKeyPath,
     sshDestinations: {
         test: {
